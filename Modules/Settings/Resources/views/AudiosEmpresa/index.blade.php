f9a51c705949d3e12d356f2684ba86a36c1dcfec
Ivan Sanchez Vallejo <ivan.sanchez@ipbtel.local>
2019-10-29 13:28:19 -0600

    Correccion modulo de Audios

--- Modules/Settings/Resources/views/AudiosEmpresa/index.blade.php
+++ Modules/Settings/Resources/views/AudiosEmpresa/index.blade.php
@@ -26,8 +26,8 @@
                         @foreach( $audios as $audio )
                             <tr data-id="{{ $audio->id }}" style="cursor:pointer">
                                 <td>{{ $audio->nombre }}</td>
-                                <td>{{ $audio->descripcion }}</td>                                                                                               <!-- strtok nos sirve para dividir nuestra ruta y nombre de archivo y asi solo tener el nombre y la extension del archivo para posteriormente reproducirla -->
-                                <td><audio controls preload="metadata"><source src='http://localhost/Nimbus/storage/app/public/audios/{{ $audio->Empresas_id }}/<?php strtok($audio->ruta,'/'); echo strtok('/'); ?>' type="audio/wav">Tu navegador no soporta audio, favor de obtener la ultima version</audio></td>
+                                <td>{{ $audio->descripcion }}</td>                                                                                            <!-- strtok nos sirve para dividir nuestra ruta y nombre de archivo y asi solo tener el nombre y la extension del archivo para posteriormente reproducirla -->
+                                <td><audio controls preload="metadata"><source src='http://10.255.242.136/audios/temp/{{ $audio->ruta }}' type="audio/wav">Tu navegador no soporta audio, favor de obtener la ultima version</audio></td>
                             </tr>
                         @endforeach
                     </tbody>
