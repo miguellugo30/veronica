143f00a276265b92522f88e6db816f2ad1c2428a
Ivan Sanchez Vallejo <ivan.sanchez@ipbtel.local>
2020-02-17 10:38:03 -0600

    Mejora Catalogo de Campos para Plantillas

--- Modules/Administrador/Resources/views/cat_campos_plantillas/edit.blade.php
+++ Modules/Administrador/Resources/views/cat_campos_plantillas/edit.blade.php
@@ -8,18 +8,18 @@
     <label for="empresa">Selecciona la(s) Empresa(s)</label>
         <div class="form-row col-md-12">
             <div class="col-md-5">
-                <select name="empresa" id="empresa" class="form-control form-control-lg" multiple>
+                <select name="empresa" id="empresa" class="form-control form-control-sm" size="15" multiple>
                     @foreach( $Empresas2 as $empresa2 )
                     <option value="{{ $empresa2->id }}">{{ $empresa2->nombre }}</option>
                     @endforeach
                 </select>
             </div>
-            <div class="col-md-2">
-                <button type="button" class="btn btn-success btn-lg activate btnRight"><i class="fas fa-forward"></i></button>
-                <button type="button" class="btn btn-danger btn-lg activate btnLeft"><i class="fas fa-backward"></i></button>
+            <div class="col-md-2 btn-group-vertical">
+                <button type="button" class="btn activate btnRight"><i class="fas fa-forward"></i></button>
+                <button type="button" class="btn activate btnLeft"><i class="fas fa-backward"></i></button>
             </div>
             <div class="col-md-5">
-                <select name="empresaAdd" id="empresaAdd" class="form-control form-control-lg" multiple>
+                <select name="empresaAdd" id="empresaAdd" class="form-control form-control-sm" size="15" multiple>
                     @foreach( $Empresas as $empresa )
                     <option value="{{ $empresa->id }}">{{ $empresa->nombre }}</option>
                     @endforeach
